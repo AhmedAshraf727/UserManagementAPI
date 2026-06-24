9cab74e0b8dd9d2b6ea182657ac3dc529f102f5f
Ahmed Ashraf <134469174+AhmedAshraf727@users.noreply.github.com>
2026-06-24 09:01:37 +0200

    Add project files.

---
+++ UserManagementAPI/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+    }
+}
